50dbce560c508d679ad03f138cc21e56a0f3cefb
David Serrao <davidmserrao@gmail.com>
2024-09-28 00:31:37 -0400

    Initial Commit

---
+++ SocialSpark/SocialSparkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialSparkApp.swift
+//  SocialSpark
+//
+//  Created by David Serrao on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SocialSparkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
