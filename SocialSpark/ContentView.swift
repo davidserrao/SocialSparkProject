d92eec9717400b081150b7e80cf505d63577bef6
B-Ricey763 <bhanna30@gatech.edu>
2024-09-29 02:50:06 -0400

    stuff

--- SocialSpark/ContentView.swift
+++ SocialSpark/ContentView.swift
@@ -47,44 +47,10 @@ struct ContentView: View {
             if success, let token = token {
                 self.isAuthenticated = true
                 self.accessToken = token
+                
             }
         }
     }
-    
-    func fetchUserInfo(accessToken: String) {
-        // Auth0 userinfo endpoint
-        let url = URL(string: "https://<YOUR_DOMAIN>.auth0.com/userinfo")!
-        
-        // Create a URL request with the access token in the Authorization header
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        request.setValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
-        
-        // Create a data task to fetch user info
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            // Handle error
-            if let error = error {
-                print("Error: \(error.localizedDescription)")
-                return
-            }
-            
-            // Handle response and data
-            if let data = data {
-                do {
-                    // Try to decode JSON response into a dictionary
-                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                        print("User Info: \(json)")
-                    }
-                } catch {
-                    print("Failed to parse user info: \(error.localizedDescription)")
-                }
-            }
-        }
-        
-        // Start the request
-        task.resume()
-    }
-
 }
 
 #Preview {
